1997ce604cc179e33f0cdf695fb21063ad637a53
Nilesh Raut <nileshdr301@gmail.com>
2026-08-11 20:48:47 +0530

    Add files via upload

---
+++ one dimensional array declaration and initialize.c
@@ -0,0 +1,8 @@
+// 1 dimensional array declaration and initialize
+#include<stdio.h>
+int main()
+{
+	int marks[5]={ 90,80,70,60,50};
+	printf("The first element in array is : %d", marks[0]);  // 1st element
+	return 0;
+}
